fedd60234251825916d373a878fed45fde74ec77
Marco Sieben <57383943+sieben-marco@users.noreply.github.com>
2024-11-18 23:37:41 +0000

    corrigido main

--- main.c
+++ main.c
@@ -1,24 +1,5 @@
 #include <stdio.h>
 
-typedef struct {
-    char nome[50];
-    char email[50];
-    char telefone[15];
-    char cpf[15];
-} contato;
-
-typedef struct {
-    contato contatos;
-    struct elo* proximo;
-    struct elo* anterior;
-} elo;
-
-typedef struct {
-    elo* inicio;
-    elo* fim;
-    int tamanho;
-} lista;
-
 #include "main.h"
 int main(){
     int operacao;
