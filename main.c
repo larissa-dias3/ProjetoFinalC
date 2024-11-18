8a7ca634452811f3baf05f41f977112c16067c25
Marco Sieben <57383943+sieben-marco@users.noreply.github.com>
2024-11-18 23:08:34 +0000

    Adicionadas structs para a lista de contato e header

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+
+typedef struct {
+    char nome[50];
+    char email[50];
+    char telefone[15];
+    char cpf[15];
+} contato;
+
+typedef struct {
+    contato *contatos;
+    int prox;
+    int ant;
+} elo;
+
 int main(){
     return 0;
 }
