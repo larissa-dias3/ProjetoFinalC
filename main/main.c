98f6c0f3ae835a458d1200cf65a039609c6069d4
Marco Sieben <57383943+sieben-marco@users.noreply.github.com>
2024-11-23 21:01:27 +0000

    main

--- main/main.c
+++ main/main.c
@@ -23,7 +23,7 @@ int main(){
     return 0;
 }
 //funcoes
-void inicializar (t_headerLista *lista){
+void inicializar(t_headerLista *lista){
     lista->inicio = NULL;
     lista->fim = NULL;
     lista->tamanho = 0;
@@ -33,20 +33,32 @@ int vazia(t_headerLista *lista){
     return (lista->inicio == NULL);
 }
 
-void insereContato(t_contato contato){
+void insereContato(t_headerLista *fila, t_contato contato){
+    t_elo *novo_elo = (t_elo*) malloc(sizeof(t_elo)); // aloca espaço para o novo elo
 
-    printf("Informe o nome do contato: ");
-    gets(contato.nome);
+    if (novo_elo == NULL) { // se não conseguir alocar
+        return 0;
+    }
 
-    printf("Informe o email do contato: ");
-    gets(contato.nome);
+    // t_contato novo_contato;
 
-    printf("Informe o telefone do contato: ");
-    scanf("%s", &contato.telefone);
+    // printf("Informe o nome do contato: ");
+    // gets(novo_contato.nome);
 
-    printf("Informe o cpf do contato: ");
-    scanf("%s", &contato.cpf);
+    // printf("Informe o email do contato: ");
+    // gets(novo_contato.email);
 
+    // printf("Informe o telefone do contato: ");
+    // gets(novo_contato.telefone);
+
+    // printf("Informe o cpf do contato: ");
+    // gets(novo_contato.cpf);
+
+    novo_elo->contato = contato;
+    novo_elo->anterior = fila->fim;
+    
+
+    return 1;
 }
 void menu(){
     printf("--Menu de interações--");
